af77fd12f17d05c88de63fb2b85cfdb7c6bd4b2b
Samueelx <gsammygitau@gmail.com>
2022-11-27 12:48:50 +0300

    Completed assignment.

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = (num) => num ** 2;
+
+const add = (param1, param2) => param1 + param2;
+
+console.log(`Divide: ${divide()}
+Square: ${square(7)}
+Add: ${add(7, 9)}
+`);
